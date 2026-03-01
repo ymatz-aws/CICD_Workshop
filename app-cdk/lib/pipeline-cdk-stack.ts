e9285d9c2b964638cea0e8c247cb7233c767e979
ymatz <ymatz@amazon.co.jp>
2026-03-01 15:52:15 +0000

    Implemented CI pipeline

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -1,6 +1,9 @@
 import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { aws_codeconnections as codeconnections } from 'aws-cdk-lib';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
 
 export class PipelineCdkStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
@@ -9,14 +12,60 @@ export class PipelineCdkStack extends Stack {
     const SourceConnection = new codeconnections.CfnConnection(this, 'CICD_Workshop_Connection', {
         connectionName: 'CICD_Workshop_Connection',
         providerType: 'GitHub',
-      });
+    });
+
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      pipelineName: 'CICD_Pipeline',
+      crossAccountKeys: false,
+      pipelineType: codepipeline.PipelineType.V2,
+      executionMode: codepipeline.ExecutionMode.QUEUED,
+    });
+
+    const codeBuild = new codebuild.PipelineProject(this, 'CodeBuild', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+        privileged: true,
+        computeType: codebuild.ComputeType.LARGE,
+      },
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_test.yml'),
+    }
+  );
+
+    const sourceOutput = new codepipeline.Artifact();
+    const unitTestOutput = new codepipeline.Artifact();
+
+    pipeline.addStage({
+        stageName: 'Source',
+        actions: [
+          new codepipeline_actions.CodeStarConnectionsSourceAction({
+            actionName: 'GitHub',
+            owner: 'ymatz-aws',
+            repo: 'CICD_Workshop',
+            output: sourceOutput,
+            branch: 'main',
+            connectionArn: 'arn:aws:codeconnections:ap-northeast-1:041967461206:connection/35f4df1d-cfc7-4e72-bead-d838d0e51fad',
+          }),
+        ],
+    });
+
+    pipeline.addStage({
+      stageName: 'Code-Quality-Testing',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Unit-Test',
+          project: codeBuild,
+          input: sourceOutput,
+          outputs: [unitTestOutput],
+        }),
+      ],
+    });
 
     new CfnOutput(this, 'SourceConnectionArn', {
         value: SourceConnection.attrConnectionArn,
-      });
+    });
 
     new CfnOutput(this, 'SourceConnectionStatus', {
         value: SourceConnection.attrConnectionStatus,
-      });
+    });
   }
 }
--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,15 +1,19 @@
 /// <reference types='vite/client' />
+/// <reference types='vitest' />
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [react()],
   base: '/my-app',
   server: {
-    allowedHosts: true,
     host: true,
     port: 8081,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
   },
 })
