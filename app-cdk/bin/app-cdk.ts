8944fca7fa1f1a3e00b6211076e2908940dba036
ymatz <ymatz@amazon.co.jp>
2026-03-02 13:36:19 +0000

    Added Test environment

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -1,14 +1,16 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AppCdkStack } from '../lib/app-cdk-stack';
-import { PipelineCdkStack } from '../lib/pipeline-cdk-stack';
+import { PipelineCdkStack } from '../lib/pipeline-cdk-stack'
 import { EcrCdkStack } from '../lib/ecr-cdk-stack';
 
 const app = new cdk.App();
 
 const ecrCdkStack = new EcrCdkStack(app, 'ecr-stack', {});
 
-const testCdkStack = new AppCdkStack(app, 'test', {});
+const testCdkStack = new AppCdkStack(app, 'test', {
+  ecrRepository: ecrCdkStack.repository,
+});
 
 const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {
   ecrRepository: ecrCdkStack.repository,
