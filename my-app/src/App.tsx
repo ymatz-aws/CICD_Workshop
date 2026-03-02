ae3384e507d260970fa3ce1eda1d3b8be443ff99
ymatz <ymatz@amazon.co.jp>
2026-03-02 16:34:44 +0000

    Added Blue/Green Prod delivery

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <p>CI/CD TEST</p>
+      <p>Thisis Green!!</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
